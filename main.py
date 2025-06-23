9dde164b4ae01a29b22aa8e288157c12ff72d1b5
Charan6924 <cxv166@case.edu>
2025-06-23 10:54:43 +0530

    added rag egine

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from rag_engine import generate_answer
+
+app = FastAPI()
+
+class QueryRequest(BaseModel):
+    query: str
+
+@app.post("/rag")
+def rag_endpoint(request: QueryRequest):
+    answer = generate_answer(request.query)
+    return {"answer": answer}
